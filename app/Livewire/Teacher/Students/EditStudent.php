6a3a1191b06921c1afa3364dc51c7538cd0e8aa2
mhmdshiddiq17 <muhammad.shiddiq@berijalan.net>
2025-06-18 10:32:59 +0700

    DEVELOP-16JUNE: Students Crud

---
+++ app/Livewire/Teacher/Students/EditStudent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Teacher\Students;
+
+use Livewire\Component;
+
+class EditStudent extends Component
+{
+    public function render()
+    {
+        return view('livewire.teacher.students.edit-student');
+    }
+}
