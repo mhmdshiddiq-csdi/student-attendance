7e9394c4363d25adeee6e46e058c4a060cef76dc
mhmdshiddiq17 <muhammad.shiddiq@berijalan.net>
2025-06-18 12:20:19 +0700

    adsadsa

--- app/Livewire/Teacher/Students/StudentList.php
+++ app/Livewire/Teacher/Students/StudentList.php
@@ -4,9 +4,16 @@ namespace App\Livewire\Teacher\Students;
 
 use App\Models\Student;
 use Livewire\Component;
+use Masmerise\Toaster\Toaster;
 
 class StudentList extends Component
 {
+    public function delete($id) 
+    {
+        Student::find($id)->delete();
+        Toaster::success('student deleted successfully');
+        return redirect()->route('student.index');
+    }
     public function render()
     {
         return view('livewire.teacher.students.student-list', [
